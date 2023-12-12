ed8b42bbd26e5086cb636c7b866aad39a4ac040f
sajal <sajalsbaral@gmail.com>
2023-12-12 14:15:33 -0500

    added operate function to call the correct function when a operator is pressed

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
 
+function operate(a, b, opp) {
+    a = Number(a);
+    b = Number(b);
+
+    switch(opp) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '÷':
+            if(b == 0) {
+                alert('Cannot divide by 0!')
+            } else {
+                return divide(a,b);
+            }
+        default: 
+            return null;
+    }
+}
 
 function add(a,b) {
     return a + b;
